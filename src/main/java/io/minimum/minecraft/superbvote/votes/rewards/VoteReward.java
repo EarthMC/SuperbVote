2e87cd0d950818962eb517bcd7b72aac6f7958d3
Warrior <50800980+Warriorrrr@users.noreply.github.com>
2025-12-20 16:17:10 +0100

    Add a cooldown for vote messages

--- src/main/java/io/minimum/minecraft/superbvote/votes/rewards/VoteReward.java
+++ src/main/java/io/minimum/minecraft/superbvote/votes/rewards/VoteReward.java
@@ -1,5 +1,7 @@
 package io.minimum.minecraft.superbvote.votes.rewards;
 
+import com.google.common.cache.Cache;
+import com.google.common.cache.CacheBuilder;
 import io.minimum.minecraft.superbvote.configuration.SuperbVoteConfiguration;
 import io.minimum.minecraft.superbvote.configuration.message.MessageContext;
 import io.minimum.minecraft.superbvote.configuration.message.VoteMessage;
@@ -10,10 +12,13 @@ import org.bukkit.Bukkit;
 import org.bukkit.OfflinePlayer;
 import org.bukkit.entity.Player;
 
+import java.time.Duration;
 import java.util.List;
+import java.util.UUID;
 
 @Data
 public class VoteReward {
+    private static final Cache<UUID, Boolean> BROADCAST_CACHE = CacheBuilder.newBuilder().expireAfterWrite(Duration.ofHours(1)).build();
     private final String serviceName;
     private final List<RewardMatcher> rewardMatchers;
     private final List<String> commands;
@@ -26,7 +31,12 @@ public class VoteReward {
             context.getPlayer().map(OfflinePlayer::getPlayer).ifPresent(votingPlayer -> playerMessage.sendAsBroadcast(votingPlayer, context));
         }
 
-        if (broadcastMessage != null && broadcast) {
+        final UUID playerUUID = context.getPlayer().map(OfflinePlayer::getUniqueId).orElse(null);
+        if (broadcastMessage != null && broadcast && (playerUUID == null || !BROADCAST_CACHE.asMap().containsKey(playerUUID))) {
+            if (playerUUID != null) {
+                BROADCAST_CACHE.put(playerUUID, true);
+            }
+
             for (final Player player : Bukkit.getOnlinePlayers()) {
                 if (player.hasPermission("superbvote.notify")) {
                     broadcastMessage.sendAsBroadcast(player, context);
